910ebb3f9dd882155f39615d459b7b93cce8737a
Anu Sudarsan <anu.at.infy@gmail.com>
2020-03-10 09:49:19 -0400

    Add GitHub workflow for Azure tests

--- presto-hive-hadoop2/bin/run_hive_abfs_tests.sh
+++ presto-hive-hadoop2/bin/run_hive_abfs_tests.sh
@@ -8,7 +8,11 @@ test -v ABFS_CONTAINER
 test -v ABFS_ACCOUNT
 test -v ABFS_ACCESS_KEY
 
-start_docker_containers
+cleanup_hadoop_docker_containers
+start_hadoop_docker_containers
+
+test_directory="$(date '+%Y%m%d-%H%M%S')-$(uuidgen | sha1sum | cut -b 1-6)"
+test_root="abfs://${ABFS_CONTAINER}@${ABFS_ACCOUNT}.dfs.core.windows.net/${test_directory}"
 
 # insert Azure credentials
 # TODO replace core-site.xml.abfs-template with apply-site-xml-override.sh
@@ -17,6 +21,34 @@ exec_in_hadoop_master_container sed -i \
     -e "s|%ABFS_ACCESS_KEY%|${ABFS_ACCESS_KEY}|g" \
     -e "s|%ABFS_ACCOUNT%|${ABFS_ACCOUNT}|g" \
     /etc/hadoop/conf/core-site.xml
+
+# restart hive-server2 to apply changes in core-site.xml
+docker exec "$(hadoop_master_container)" supervisorctl restart hive-server2
+retry check_hadoop
+
+# create test table
+table_path="${test_root}/presto_test_external_fs/"
+exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
+exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table.csv{,.gz,.bz2,.lz4} "${table_path}"
+exec_in_hadoop_master_container /usr/bin/hive -e "CREATE EXTERNAL TABLE presto_test_external_fs(t_bigint bigint) LOCATION '${table_path}'"
+
+table_path="${test_root}/presto_test_external_fs_with_header/"
+exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
+exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header.csv{,.gz,.bz2,.lz4} "${table_path}"
+exec_in_hadoop_master_container /usr/bin/hive -e "
+    CREATE EXTERNAL TABLE presto_test_external_fs_with_header(t_bigint bigint)
+    STORED AS TEXTFILE
+    LOCATION '${table_path}'
+    TBLPROPERTIES ('skip.header.line.count'='1')"
+
+table_path="${test_root}/presto_test_external_fs_with_header_and_footer/"
+exec_in_hadoop_master_container hadoop fs -mkdir -p "${table_path}"
+exec_in_hadoop_master_container hadoop fs -copyFromLocal -f /docker/files/test_table_with_header_and_footer.csv{,.gz,.bz2,.lz4} "${table_path}"
+exec_in_hadoop_master_container /usr/bin/hive -e "
+    CREATE EXTERNAL TABLE presto_test_external_fs_with_header_and_footer(t_bigint bigint)
+    STORED AS TEXTFILE
+    LOCATION '${table_path}'
+    TBLPROPERTIES ('skip.header.line.count'='2', 'skip.footer.line.count'='2')"
 
 stop_unnecessary_hadoop_services
 
@@ -40,6 +72,6 @@ EXIT_CODE=$?
 set -e
 popd
 
-cleanup_docker_containers
+cleanup_hadoop_docker_containers
 
 exit ${EXIT_CODE}
